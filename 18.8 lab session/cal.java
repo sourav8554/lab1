53ae2656ef31df3f6994f9ef0d03258af5b5292e
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-08-18 17:40:49 +0530

    Add files via upload

---
+++ 18.8 lab session/cal.java
@@ -0,0 +1,25 @@
+
+
+
+package javaprogram2;
+
+import java.util.Scanner;
+public class cal {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input 1st integer: ");
+        int firstInt = in.nextInt();
+        System.out.print("Input 2nd integer: ");
+        int secondInt = in.nextInt();
+
+        System.out.printf("Sum of two integers: %d%n", firstInt + secondInt);
+        System.out.printf("Difference of two integers: %d%n", firstInt - secondInt);
+        System.out.printf("Product of two integers: %d%n", firstInt * secondInt);
+        System.out.printf("Average of two integers: %.2f%n", (double) (firstInt + secondInt) / 2);
+        System.out.printf("Distance of two integers: %d%n", Math.abs(firstInt - secondInt));
+        System.out.printf("Max integer: %d%n", Math.max(firstInt, secondInt));
+        System.out.printf("Min integer: %d%n", Math.min(firstInt, secondInt));
+    }
+}
