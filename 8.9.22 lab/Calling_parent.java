fc0c343f39b2cc64420b3c2345fb2c97a0f05960
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-09-09 14:23:48 +0530

    Add files via upload

---
+++ 8.9.22 lab/Calling_parent.java
@@ -0,0 +1,14 @@
+package javaprogram2;
+class parent {
+	public void a( ) {
+		System.out.println("yes we can call");
+	}
+}
+class child extends parent{
+	parent obj1 =new parent();
+}
+
+public class Calling_parent {
+	public static void main(String[] args) {
+}
+}
